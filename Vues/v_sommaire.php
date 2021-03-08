d3f46840827f92e089b45aba8d2e21dba6cc2c94
KendrickMacDonald <mcd.kendrick@gmail.com>
2021-03-08 10:53:29 +0100

    sommaire updaté

--- Vues/v_sommaire.php
+++ Vues/v_sommaire.php
@@ -10,10 +10,7 @@
         <li>
         </li>
         <li class="smenu">
-            <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
-        </li>
-        <li class="smenu">
-            <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+            <a href="index.php?uc=validerFicheFrais&action=choixInitialVisiteur" title="Valider des fiches de frais">Valider des fiches de frais</a>
         </li>
         <li class="smenu">
             <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
