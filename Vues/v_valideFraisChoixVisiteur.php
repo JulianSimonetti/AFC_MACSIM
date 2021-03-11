ca1d8f34e0894763801dbc08f1044af951502864
KendrickMacDonald <mcd.kendrick@gmail.com>
2021-03-11 11:45:21 +0100

    Choixvisiteurok

--- Vues/v_valideFraisChoixVisiteur.php
+++ Vues/v_valideFraisChoixVisiteur.php
@@ -5,7 +5,7 @@
         <form name="frmChoixVisiteurMoisFiche" id="frmChoixVisiteurMoisFiche" method="post" action="choisirVisiteurMoisFiche">
             <?php echo $pdo->ListeVisiteursDepuisRecordset(isset($_REQUEST["lstVisiteur"]) ? $_REQUEST["lstVisiteur"] : NULL); ?>
             <label for="txtMoisFiche">Mois : </label>
-            <?php echo affichageMois() ?>
+            <input type="text" name="txtMoisFiche" id="txtMoisFiche" value="<?= affichageMois()?>" readonly="readonly" />
             <input type="submit" id="btnOk" name="btnOk" value="Ok" tabindex ="20" />
         </form>
     </div>
