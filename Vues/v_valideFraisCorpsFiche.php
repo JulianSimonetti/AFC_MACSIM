59bc73fec5ba32581b7378231bda66a60cb273b3
KendrickMacDonald <mcd.kendrick@gmail.com>
2021-03-15 09:32:45 +0100

    My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.

--- Vues/v_valideFraisCorpsFiche.php
+++ Vues/v_valideFraisCorpsFiche.php
@@ -5,15 +5,16 @@
         <form name="frmChoixVisiteurMoisFiche" id="frmChoixVisiteurMoisFiche" method="post" action="index.php">
             <?php echo $pdo->ListeVisiteursDepuisRecordset(isset($_REQUEST["lstVisiteur"]) ? $_REQUEST["lstVisiteur"] : NULL); ?>
             <label for="txtMoisFiche">Mois : </label>
-            <input type="text" name="txtMoisFiche" id="txtMoisFiche" value="<?= affichageMois()?>" readonly="readonly" />
+            <input type="text" name="txtMoisFiche" id="txtMoisFiche" value="<?= affichageMois() ?>" readonly="readonly" />
             <input type="submit" id="btnOk" name="btnOk" value="Ok" tabindex ="20" />
             <input type="hidden" name="uc" value="validerFicheFrais" />
             <input type="hidden" name="action" value="afficherFicheFraisSelectionnee" />
         </form>
+        <?php
+        include("vues/v_valideFrais/v_valideFraisEtatFicheFrais.php");
+        include("vues/v_valideFrais/v_valideFraisForfait.php");
+        include("vues/v_valideFrais/v_valideFraisValider.php");
+        ?>
     </div>
-    <?php
-    include("vues/v_valideFrais/v_valideFraisEtatFicheFrais.php");
-    include("vues/v_valideFrais/v_valideFraisForfait.php");
-    include("vues/v_valideFrais/v_valideFraisValider.php");
-    ?>
+
 </body>
