b0a782db5aca97d214acd5f63a8784e9df66e39b
KendrickMacDonald <mcd.kendrick@gmail.com>
2021-03-31 08:25:59 +0200

    ghzretgzerter

--- Controleurs/c_valideFrais.php
+++ Controleurs/c_valideFrais.php
@@ -45,64 +45,65 @@ switch ($action) {
             include("vues/v_sommaire.php");
             include("vues/v_valideFraisCorpsFiche.php");
             unset($FF);
-            break;
-            case 'enregModifFF':
-            $FF = new FicheFrais($_SESSION['ff_idVisiteur'], $_SESSION['ff_mois']);
-            $FF->initInfosFicheSansFF();
-            $FF->ajouterUnFraisForfaitise('ETP', (int) $_POST['txtEtape']);
-            $FF->ajouterUnFraisForfaitise('KM', (int) $_POST['txtKm']);
-            $FF->ajouterUnFraisForfaitise('NUI', (int) $_POST['txtNuitee']);
-            $FF->ajouterUnFraisForfaitise('REP', (int) $_POST['txtRepas']);
-
-            $resMAJ = $FF->mettreAJourLesFraisForfaitises();
-
-            $etat = $FF->getLibelleEtat();
-            $nbJustificatifs = $FF->getNbJustificatifs();
-            $lesQuantites = $FF->getLesQuantitesDeFraisForfaitises();
-            $montantTotal = $FF->calculerLeMontantValide();
-
-            $lignes = $FF->getLesFraisForfaitises();
-
-            $etp = $lignes['1']->getQuantite();
-            $km = $lignes['2']->getQuantite();
-            $nui = $lignes['3']->getQuantite();
-            $rep = $lignes['4']->getQuantite();
-
-            include("vues/v_sommaire.php");
-            include("vues/v_valideFraisCorpsFiche.php");
-            break;
-            case 'enregModifFHF':
-            $FF = new FicheFrais($_SESSION['ff_idVisiteur'], $_SESSION['ff_mois']);
-            $FF->initAvecInfosBDDSansFHF();
-
-            $FF->ajouterUnFraisHorsForfait((int) $_POST['numFHF'], $_POST['txtLibelle'], $_POST['txtDate'], (float) $_POST['txtMontant'], $_POST['fraisAction']);
-
-            if ($FF->controlerNbJustificatifs()) {
-                $resMAJ = $FF->mettreAJourLesFraisHorsForfait();
-            } else {
-                $resMAJ = "Le nombre de justificatifs est incorrect.";
-            }
-
-            $lignes = $FF->getLesFraisForfaitises();
-
-            $lesFHF = $FF->getLesInfosFraisHorsForfait();
-
-
-            $etp = $lignes['1']->getQuantite();
-            $km = $lignes['2']->getQuantite();
-            $nui = $lignes['3']->getQuantite();
-            $rep = $lignes['4']->getQuantite();
-
-            $etat = $FF->getLibelleEtat();
-            $nbJustificatifs = $FF->getNbJustificatifs();
-            $lesQuantites = $FF->getLesQuantitesDeFraisForfaitises();
-            $montantTotal = $FF->calculerLeMontantValide();
-
-
-            include("vues/v_sommaire.php");
-            include("vues/v_valideFraisCorpsFiche.php");
-            unset($FF);
         }
+        break;
+    case 'enregModifFF':
+        $FF = new FicheFrais($_SESSION['ff_idVisiteur'], $_SESSION['ff_mois']);
+        $FF->initInfosFicheSansFF();
+        $FF->ajouterUnFraisForfaitise('ETP', (int) $_POST['txtEtape']);
+        $FF->ajouterUnFraisForfaitise('KM', (int) $_POST['txtKm']);
+        $FF->ajouterUnFraisForfaitise('NUI', (int) $_POST['txtNuitee']);
+        $FF->ajouterUnFraisForfaitise('REP', (int) $_POST['txtRepas']);
+
+        $resMAJ = $FF->mettreAJourLesFraisForfaitises();
+
+        $etat = $FF->getLibelleEtat();
+        $nbJustificatifs = $FF->getNbJustificatifs();
+        $lesQuantites = $FF->getLesQuantitesDeFraisForfaitises();
+        $montantTotal = $FF->calculerLeMontantValide();
+
+        $lignes = $FF->getLesFraisForfaitises();
+
+        $etp = $lignes['1']->getQuantite();
+        $km = $lignes['2']->getQuantite();
+        $nui = $lignes['3']->getQuantite();
+        $rep = $lignes['4']->getQuantite();
+
+        include("vues/v_sommaire.php");
+        include("vues/v_valideFraisCorpsFiche.php");
+        break;
+    case 'enregModifFHF':
+        $FF = new FicheFrais($_SESSION['ff_idVisiteur'], $_SESSION['ff_mois']);
+        $FF->initAvecInfosBDDSansFHF();
+
+        $FF->ajouterUnFraisHorsForfait((int) $_POST['numFHF'], $_POST['txtLibelle'], $_POST['txtDate'], (float) $_POST['txtMontant'], $_POST['fraisAction']);
+
+        if ($FF->controlerNbJustificatifs()) {
+            $resMAJ = $FF->mettreAJourLesFraisHorsForfait();
+        } else {
+            $resMAJ = "Le nombre de justificatifs est incorrect.";
+        }
+
+        $lignes = $FF->getLesFraisForfaitises();
+
+        $lesFHF = $FF->getLesInfosFraisHorsForfait();
+
+
+        $etp = $lignes['1']->getQuantite();
+        $km = $lignes['2']->getQuantite();
+        $nui = $lignes['3']->getQuantite();
+        $rep = $lignes['4']->getQuantite();
+
+        $etat = $FF->getLibelleEtat();
+        $nbJustificatifs = $FF->getNbJustificatifs();
+        $lesQuantites = $FF->getLesQuantitesDeFraisForfaitises();
+        $montantTotal = $FF->calculerLeMontantValide();
+
+
+        include("vues/v_sommaire.php");
+        include("vues/v_valideFraisCorpsFiche.php");
+        unset($FF);
+
         break;
 
     case 'validerFicheFrais':
